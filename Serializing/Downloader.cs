8e87b9ed0b9ac57790558f36c862a3dd658579cb
Yannik Seitz <yannikseitz@outlook.de>
2020-03-05 19:03:48 +0100

    1

--- Serializing/Downloader.cs
+++ Serializing/Downloader.cs
@@ -10,54 +10,145 @@ namespace HelperClasses
 {
 	public class Downloader
 	{
+		public void DownloadImageGoodReads(string mediaName, string target)
+		{
+			string source = FindImageGoodReads(mediaName.Replace(" ", "+"));
+
+			using (WebClient client = new WebClient())
+			{
+				client.Headers.Add("User-Agent: Other");
+				client.DownloadFile(new Uri(source), target);
+			}
+		}
+
+		public void DownloadImageNovelUpdates(string mediaName, string target)
+		{
+			string source = FindImageNovelUpdates(mediaName.Replace(" ", "+"));
+
+			using (WebClient client = new WebClient())
+			{
+				client.Headers.Add("User-Agent: Other");
+				client.DownloadFile(new Uri(source), target);
+			}
+		}
+
 		public void DownloadImageIMDB(string mediaName, string target)
 		{
 			string source = FindImageIMDB(mediaName.Replace(" ", "+"));
 
 			using (WebClient client = new WebClient())
 			{
+				client.Headers.Add("User-Agent: Other");
 				client.DownloadFile(new Uri(source), target);
-				// OR 
-				//client.DownloadFileAsync(new Uri(url), @"c:\temp\image35.png");
 			}
+		}
+
+
+
+
+
+		private string FindImageNovelUpdates(string url)
+		{
+			string targetUrl = "https://www.novelupdates.com/?s=" + url + "&post_type=seriesplans";
+
+			WebClient client = new WebClient();
+			client.Headers.Add("User-Agent: Other");
+			string html = client.DownloadString(targetUrl);
+
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml(html);
+
+			List<HtmlNode> imageNodes = null;
+			imageNodes = (from HtmlNode node in doc.DocumentNode.SelectNodes("//a")
+						  where node.Name == "a" && node.ParentNode.Attributes["class"] != null && node.Attributes["href"] != null
+						  select node).ToList();
+
+			client = new WebClient();
+			client.Headers.Add("User-Agent: Other");
+			html = client.DownloadString(imageNodes[12].Attributes["href"].Value.ToString());
+
+			doc = new HtmlDocument();
+			doc.LoadHtml(html);
+
+			imageNodes = null;
+			imageNodes = (from HtmlNode node in doc.DocumentNode.SelectNodes("//img")
+						  where node.Name == "img" && node.ParentNode.Attributes["class"] != null && node.Attributes["src"] != null
+						  select node).ToList();
+
+
+			if (imageNodes[0] != null)
+			{
+				string input = imageNodes[0].Attributes["src"].Value.ToString();				
+
+				return input;
+			}
+
+			return null;
+		}
+
+
+		private string FindImageGoodReads(string url)
+		{
+			string targetUrl = "https://www.goodreads.com/search?utf8=✓&q=" + url;
+
+			WebClient client = new WebClient();
+			client.Headers.Add("User-Agent: Other");
+			string html = client.DownloadString(targetUrl);
+
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml(html);
+
+			List<HtmlNode> imageNodes = null;
+			imageNodes = (from HtmlNode node in doc.DocumentNode.SelectNodes("//img")
+						  where node.Name == "img" && node.Attributes["class"] != null && node.Attributes["src"] != null
+						  select node).ToList();
+
+			if (imageNodes[0] != null)
+			{
+				string input = imageNodes[0].Attributes["src"].Value.ToString();
+
+				int index = input.IndexOf("._");
+				if (index > 0)
+				{
+					input = input.Substring(0, index + 2);
+				}
+
+				return (input + ".jpg");
+			}
+
+			return null;
 		}
 
 		private string FindImageIMDB(string url)
 		{
-			//string targetUrl = "https://www.imdb.com/find?q=ghost+in+the+shell+(1995)";
 			string targetUrl = "https://www.imdb.com/find?q=" + url;
-			Console.WriteLine(targetUrl);
 
-			// For speed of dev, I use a WebClient
 			WebClient client = new WebClient();
+			client.Headers.Add("User-Agent: Other");
 			string html = client.DownloadString(targetUrl);
 
-			// Load the Html into the agility pack
 			HtmlDocument doc = new HtmlDocument();
 			doc.LoadHtml(html);
 
-			// Now, using LINQ to get all Images
 			List<HtmlNode> imageNodes = null;
 			imageNodes = (from HtmlNode node in doc.DocumentNode.SelectNodes("//img")
-						  where node.Name == "img" && node.ParentNode.Attributes["href"] != null
+						  where node.Name == "img" && node.ParentNode.Attributes["href"] != null && node.Attributes["src"] != null
 						  select node).ToList();
 
-
-			foreach (HtmlNode node in imageNodes)
+			if (imageNodes[0] != null)
 			{
-				Console.WriteLine(node.Attributes["src"].Value);
+				string input = imageNodes[0].Attributes["src"].Value.ToString();
+
+				int index = input.IndexOf("_V1_");
+				if (index > 0)
+				{
+					input = input.Substring(0, index + 4);
+				}
+
+				return (input + ".jpg");
 			}
 
-
-			string input = imageNodes[0].Attributes["src"].Value.ToString();
-
-			int index = input.IndexOf("_V1_");
-			if (index > 0)
-			{
-				input = input.Substring(0, index + 4);
-			}
-
-			return (input + ".jpg");
+			return null;
 		}
 	}
 }
